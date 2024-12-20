619db60347afc6dc89687b6a65bba5283fa71ba9
Enrique Mascarua <lahey1991@gmail.com>
2024-12-20 13:07:34 -0600

    WIP:calendar class methods

--- client/src/class/Calendar.tsx
+++ client/src/class/Calendar.tsx
@@ -15,6 +15,29 @@ export class Calendar {
     }
 
     renderCalendar(){
-        
+        let count = 0
+        let week = []
+        const mes = []
+
+        for(let i = 0; i < this.espaciosPrimeros; i ++){
+            week.push("")
+            count++
+        }
+
+        for(let i = 1; i <= this.contaDeDias; i ++){
+            if(count != 7){
+                week.push(i)
+            } else {
+                mes.push(week)
+                count = 0
+                week = []
+            }
+        } 
+
+        for(let i = 0; i < this.espaciosFinales; i ++){
+            week.push("")
+        }
+
+        return mes
     }
 }
